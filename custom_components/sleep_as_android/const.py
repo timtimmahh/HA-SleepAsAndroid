2d5600fbef278b0186ca86890dedb71fc2f4ba32
Timothy Logan <timtimmahh@gmail.com>
2022-04-01 06:16:09 -0400

    Modified config flow and manifest

--- custom_components/sleep_as_android/const.py
+++ custom_components/sleep_as_android/const.py
@@ -6,3 +6,13 @@ DEVICE_MACRO: str = "%%%device%%%"
 DEFAULT_NAME = "SleepAsAndroid"
 DEFAULT_TOPIC_TEMPLATE = "SleepAsAndroid/%s" % DEVICE_MACRO
 DEFAULT_QOS = 0
+DEFAULT_ALARM_LABEL = ""
+
+CONF_ALARMS = "alarms"
+CONF_ALARM_TIME_FMT = "%I:%M %p"
+CONF_ALARM_DATE_FMT = "%a, %b %d, %Y"
+CONF_ALARM_LABEL = "label"
+CONF_ALARM_TIME = "time"
+CONF_ALARM_DATE = "date"
+CONF_ALARM_REPEAT = "repeat"
+CONF_ALARM_ADD_ANOTHER = "add_another"
