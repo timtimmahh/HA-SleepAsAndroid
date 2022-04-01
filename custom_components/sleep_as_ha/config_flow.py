665f180498c7e2ae3a20d1a42ca04f4398ce5f90
Timothy Logan <timtimmahh@gmail.com>
2022-04-01 05:03:26 -0400

    Modified config flow and manifest

--- custom_components/sleep_as_ha/config_flow.py
+++ custom_components/sleep_as_ha/config_flow.py
@@ -1,11 +1,17 @@
 """Configuration via UI for the integration."""
 from __future__ import annotations
 
+from datetime import datetime
+from typing import Optional, Dict, Any
+
 from homeassistant import config_entries
 from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
-from .const import DEFAULT_NAME, DEFAULT_QOS, DEFAULT_TOPIC_TEMPLATE, DOMAIN
+from .const import DEFAULT_NAME, DEFAULT_QOS, DEFAULT_TOPIC_TEMPLATE, DOMAIN, CONF_ALARMS, \
+    DEFAULT_ALARM_LABEL, CONF_ALARM_TIME_FMT, CONF_ALARM_DATE_FMT, CONF_ALARM_LABEL, \
+    CONF_ALARM_TIME, CONF_ALARM_DATE, CONF_ALARM_REPEAT, CONF_ALARM_ADD_ANOTHER
 
 
 def get_value(config_entry: config_entries | None, param: str, default=None):
@@ -23,7 +29,7 @@ def get_value(config_entry: config_entries | None, param: str, default=None):
 
 
 def create_schema(
-    config_entry: config_entries | None, step: str = "user"
+        config_entry: config_entries | None, step: str = "user"
 ) -> vol.Schema:
     """Generate configuration schema.
 
@@ -39,37 +45,54 @@ def create_schema(
                     default=get_value(
                         config_entry=config_entry, param="name", default=DEFAULT_NAME
                     ),
-                ): str,
+                ): cv.string,
             }
         )
 
-    schema = schema.extend(
-        {
-            vol.Required(
-                "topic_template",
-                default=get_value(
-                    config_entry=config_entry,
-                    param="topic_template",
-                    default=DEFAULT_TOPIC_TEMPLATE,
-                ),
-            ): str,
-            vol.Optional(
-                "qos",
-                default=get_value(
-                    config_entry=config_entry, param="qos", default=DEFAULT_QOS
-                ),
-            ): int,
-        }
-    )
+    if step != "alarm":
+        schema = schema.extend(
+            {
+                vol.Required(
+                    "topic_template",
+                    default=get_value(
+                        config_entry=config_entry,
+                        param="topic_template",
+                        default=DEFAULT_TOPIC_TEMPLATE,
+                    ),
+                ): cv.string,
+                vol.Optional(
+                    "qos",
+                    default=get_value(
+                        config_entry=config_entry, param="qos", default=DEFAULT_QOS
+                    ),
+                ): int,
+            }
+        )
+    else:
+        schema = schema.extend(
+            {
+                vol.Optional(CONF_ALARM_LABEL, default=""): cv.string,
+                vol.Required(CONF_ALARM_TIME, default=datetime.now().strftime(CONF_ALARM_TIME_FMT)):
+                    cv.time,
+                vol.Required(CONF_ALARM_DATE, default=datetime.now().strftime(CONF_ALARM_DATE_FMT)):
+                    vol.Datetime(CONF_ALARM_DATE_FMT),
+                vol.Required(CONF_ALARM_REPEAT): cv.multi_select({
+                    "Sunday": False,
+                    "Monday": False,
+                    "Tuesday": False,
+                    "Wednesday": False,
+                    "Thursday": False,
+                    "Friday": False,
+                    "Saturday": False,
+                }),
+                vol.Optional(CONF_ALARM_ADD_ANOTHER): cv.boolean
+            }
+        )
     return schema
 
 
 class SleepAsAndroidConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """First time set up flow."""
-
-    def __init__(self):
-        """Initialize entry."""
-        self._config_entry: config_entries | None = None
 
     @staticmethod
     @callback
@@ -80,11 +103,31 @@ class SleepAsAndroidConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
         if user_input is not None:
-            return self.async_create_entry(title=user_input["name"], data=user_input)
+            self.data = user_input
+            self.data[CONF_ALARMS] = []
+            return await self.async_step_alarm()
+            # self.async_create_entry(title=user_input["name"], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=create_schema(None, step="user")
         )
+
+    async def async_step_alarm(self, user_input: Optional[Dict[str, Any]] = None):
+        """Second step in config flow to add alarms."""
+        if user_input is not None:
+            self.data[CONF_ALARMS].append({
+                "label": user_input[CONF_ALARM_LABEL],
+                "time": user_input[CONF_ALARM_TIME],
+                "date": user_input[CONF_ALARM_DATE],
+                "repeat": user_input[CONF_ALARM_REPEAT],
+            })
+
+            if user_input.get(CONF_ALARM_ADD_ANOTHER, False):
+                return await self.async_step_alarm()
+
+            return self.async_create_entry(title=user_input["name"], data=self.data)
+
+        return self.async_show_form(step_id="alarm", data_schema=create_schema(None, step="alarm"))
 
 
 class SleepAsAndroidOptionsFlow(config_entries.OptionsFlow):
